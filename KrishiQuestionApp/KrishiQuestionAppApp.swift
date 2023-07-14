c2a088c033cc6a00ba05bca1e5bc075f2a19e431
Krishi <krishi3dalal@gmail.com>
2023-07-14 12:29:24 -0400

    Initial Commit

---
+++ KrishiQuestionApp/KrishiQuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrishiQuestionAppApp.swift
+//  KrishiQuestionApp
+//
+//  Created by Krishi Dalal on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct KrishiQuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
